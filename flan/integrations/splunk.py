0ae335ef40be6fb128a5430273cd407e68ad73db
Bret Lowery <bret.lowery@gmail.com>
2019-08-06 14:02:23 -0400

    Updated Splunk integration to latest client; a few bug fixes

--- flan/integrations/splunk.py
+++ flan/integrations/splunk.py
@@ -13,8 +13,9 @@ class Splunk(FlanIntegration):
     def prepare(self):
         try:
             self.service = client.connect(
+                    scheme=self.config["scheme"],
                     host=self.config["host"],
-                    port=int(self.config["port"]),
+                    port=self.config["port"],
                     username=self.config["username"],
                     password=self.config["password"])
             self.index = self.service.indexes[self.config["index"]]
@@ -28,22 +29,16 @@ class Splunk(FlanIntegration):
 
     def send(self, data):
         try:
-            self.socket.send(data)
-            if self.config["loglevel"] == "info":
-                self.loginfo('Flan->%s %s [%s]' % (self.name, "%s:%s" % (self.config["host"], self.config["port"]), self.config["index"]))
+            self.socket.send(data.encode('utf-8'))
+            self.loginfo('Flan->%s %s [%s]' % (self.name, "%s:%s" % (self.config["host"], self.config["port"]), self.config["index"]))
         except Exception as e:
-            if self.config["loglevel"] == "error":
-                self.logerr('Flan->%s delivery failed: %s' % (self.name, str(e)))
-            if self.haltonerror:
-                os._exit(1)
+            self.logerr('Flan->%s delivery failed: %s' % (self.name, str(e)))
+            pass
         return
 
     @property
     def closed(self):
-        if self.socket.closed:
-            return False
-        else:
-            return True
+        return False
 
     def close(self):
         try:
